c149fb2c21579622bab8c5273dcaa8e07d7f7190
Arvid Axelsson <arvidaxelsson1995@gmail.com>
2019-11-15 10:35:23 +0100

    ???

---
+++ CV/SkillsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SkillsViewController.swift
+//  CV
+//
+//  Created by Arvid Axelsson on 2019-11-15.
+//  Copyright © 2019 Arvid Axelsson. All rights reserved.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
